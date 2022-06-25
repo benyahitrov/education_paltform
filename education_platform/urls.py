28f9bc87ce87b6817254b64381a85af1484381d1
Evgeniy <benyahitr@gmail.com>
2022-06-25 12:31:07 +0700

    Установлен DDTB

--- education_platform/urls.py
+++ education_platform/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('base_app.urls', namespace='base_app')),
     path('admin/', admin.site.urls),
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
